57f6c158b334609482f1c142931576123b56ee1d
gpontesss <pontes.guisilva@gmail.com>
2022-11-11 15:25:46 -0300

    Fraction simplification in C with Euclid's algorithm

--- sedgewick/fraction-simplification.c
+++ sedgewick/fraction-simplification.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
 
+typedef unsigned int uint;
+typedef struct { uint num; uint den; } frac;
+
+/* Calcultates the greatest common divisor between to numbers using Euclid's
+ * algorithm. (Refer to his work, "Elements", for details.) */
+uint gcd(uint u, uint v) {
+    uint t;
+    while (u != 0) {
+        if (u < v) { t = u; u = v; v = t; }
+        u = u - v;
+    }
+    return v;
+}
+
+void print_frac(frac *x) {
+    printf("%d/%d\n", x->num, x->den);
+}
+
+void reduce_frac(frac *x) {
+    uint div = gcd(x->num, x->den);
+    (*x).num = x->num / div;
+    (*x).den = x->den / div;
+}
+
 int main() {
-    printf("hello, dummie!\n");
+    frac x = {6, 4};
+    print_frac(&x);
+    reduce_frac(&x);
+    print_frac(&x);
     return 0;
 }
